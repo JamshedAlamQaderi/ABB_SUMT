3b29ebbd44b64aaa01c7117bcca315f2a5a78469
JamshedAlamQaderi <jamshedalamqaderi@gmail.com>
2021-12-04 22:38:51 +0600

    added ui of change-password

--- frontend/src/app/change-password/change-password.component.ts
+++ frontend/src/app/change-password/change-password.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-change-password',
   templateUrl: './change-password.component.html',
-  styleUrls: ['./change-password.component.css']
+  styleUrls: ['./change-password.component.css'],
 })
 export class ChangePasswordComponent implements OnInit {
+  currentPassword: String = '';
+  newPassword: String = '';
+  confirmPassword: String = '';
 
-  constructor() { }
+  constructor(private route: Router) {
 
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void {}
+
+  goBack(){
+    this.route.navigate(['/dashboard'])
+  }
+
+  submit() {
+    // do password change api call
+    console.log(
+      'Current Password: ',
+      this.currentPassword,
+      'New Password: ',
+      this.newPassword,
+      'Confirm Password: ',
+      this.confirmPassword
+    );
+  }
 }
